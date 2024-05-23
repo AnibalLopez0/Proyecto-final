0159f55c9faa50110d26883b0a44a2a5dc1b103d
AnibalLopez0 <158408438+AnibalLopez0@users.noreply.github.com>
2024-05-22 23:24:07 -0600

    Final de final
    ya funciona exportada

--- Screens/Login2.js
+++ Screens/Login2.js
@@ -16,20 +16,20 @@ class Login2 extends Component {
 
   registrarUsuario = async (codigo, nombre) => {
     try {
-        const response = await fetch('https://new-fashioned-bulk.000webhostapp.com/Login.php', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            body: `codigo=${codigo}&nombre=${nombre}`,
-        });
+      const response = await fetch('https://new-fashioned-bulk.000webhostapp.com/Login.php', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: `codigo=${codigo}&nombre=${nombre}`,
+      });
 
-        const result = await response.text();
-        console.log(result);
+      const result = await response.text();
+      console.log(result);
     } catch (error) {
-        console.error('Error:', error);
+      console.error('Error:', error);
     }
-};
+  };
 
   sendData = async () => {
     const { codigo, nip } = this.state;
@@ -39,32 +39,34 @@ class Login2 extends Component {
 
     // Guardar el código, el NIP y la hora de creación en AsyncStorage
     try {
-      const http = new XMLHttpRequest();
-      const url = 'http://148.202.152.33/cucei/autentificacion_siauu_temporal.php';
-      const params = `codigo=${codigo}&nip=${nip}`;
-      
-      http.open('POST', url, true);
-      http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-      http.onreadystatechange = async () => {
-        if (http.readyState == 4 && http.status == 200) {
-          console.log(http.responseText);
-          await AsyncStorage.setItem('codigo', codigo);
-          await AsyncStorage.setItem('nip', nip);
-          await AsyncStorage.setItem('horaCreacion', horaCreacion);
-          const separado = http.responseText.split(',');
-          await AsyncStorage.setItem('nombre', separado[2]);
-          await AsyncStorage.setItem('carrera', separado[3]);
-          await AsyncStorage.setItem('campus', separado[4]);
+      const response = await fetch('https://new-fashioned-bulk.000webhostapp.com/Codigo.php', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: `codigo=${codigo}&nip=${nip}`,
+      });
 
-          //llamar registrar usuario
-          await this.registrarUsuario(codigo,separado[2]);
-          // Actualizar el estado de autenticación después de guardar los datos en AsyncStorage
-          this.setState({ isAuthenticated: true });
-          RNRestart.Restart();
-          
-        }
-      };
-      http.send(params);
+      if (response.ok) {
+        const result = await response.text();
+        console.log(result);
+        await AsyncStorage.setItem('codigo', codigo);
+        await AsyncStorage.setItem('nip', nip);
+        await AsyncStorage.setItem('horaCreacion', horaCreacion);
+
+        const separado = result.split(',');
+        await AsyncStorage.setItem('nombre', separado[2]);
+        await AsyncStorage.setItem('carrera', separado[3]);
+        await AsyncStorage.setItem('campus', separado[4]);
+
+        // llamar registrarUsuario
+        await this.registrarUsuario(codigo, separado[2]);
+        // Actualizar el estado de autenticación después de guardar los datos en AsyncStorage
+        this.setState({ isAuthenticated: true });
+        RNRestart.Restart();
+      } else {
+        console.error('Error en la solicitud al servicio externo:', response.statusText);
+      }
     } catch (error) {
       console.error('Error al enviar los datos:', error);
     }
